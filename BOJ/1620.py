120e78949e6049ae03eb31408eb953c3a39c7944
Kim Daehee <fabiano7@naver.com>
2020-12-08 00:21:57 +0900

    update: BOJ #1620, 나는야 포켓몬 마스터 이다솜

---
+++ BOJ/1620.py
@@ -0,0 +1,13 @@
+import sys
+input = sys.stdin.readline
+
+N, M = map(int, input().split())
+
+dic = {}
+for i in range(1, N+1):
+    name = input().rstrip()
+    dic[name] = str(i)
+    dic[str(i)] = name
+
+for _ in range(M):
+    print(dic[input().rstrip()])
