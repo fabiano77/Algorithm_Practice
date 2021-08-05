d390b3e128c0369e0e78feee4625d3284c1438ad
fabiano77 <fabiano7@naver.com>
2021-08-05 20:23:27 +0900

    update: BOJ #2417, (이분탐색) 정수 제곱근

---
+++ BOJ/2417.py
@@ -0,0 +1,13 @@
+n = int(input())
+
+start = 0
+end = n
+ans = 0
+while start <= end:
+    mid = (start + end)//2
+    if mid*mid >= n:
+        end = mid - 1
+        ans = mid
+    else:
+        start = mid + 1
+print(ans)
